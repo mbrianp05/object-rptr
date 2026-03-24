c5420de15e7583284a8e2a42da4ca641d1a62c1c
Brian <mbrianp05@gmail.com>
2026-03-24 15:24:59 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,81 @@
+from typing import List, Any
+
+class Person:
+  def __init__(self, name: str, lastname: str, group: int, team: int):
+    self.__name = name
+    self.__lastname = lastname
+
+    self.setGroup(group)
+    self.setTeam(team)
+
+  # El nombre y el apellidos son inmutables para la mayoria de logicas de negocio
+  # Por lo que se restringe el acceso a dichas props 
+
+  def getName(self) -> str:
+    return self.__name
+
+  def getLastname(self) -> str:
+    return self.__lastname
+  
+  # El grupo y el equipo pueden tener varias validaciones lógicas 
+  # por eso los setters y getters
+
+  def setGroup(self, group: int):
+    if group < 11:
+      raise Exception("El número del grupo debe ser como mínimo 11")
+
+    group = str(group)
+
+    if len(group) != 2:
+      raise Exception("El grupo debe tener 2 cifras")
+
+    if int(group[0]) > 4:
+      raise Exception("El grupo empieza por el año por lo que la primera cifra no puede ser mayor que 4")
+
+    if int(group[1]) == 0:
+      raise Exception("El segundo dígito del grupo debe ser mayor que 0")
+
+    self.__group = group
+
+  def getGroup(self) -> int:
+    return self.__group
+  
+  def setTeam(self, team: int):
+    if team < 1:
+      raise Exception("El número del equipo debe ser como mínimo 1")
+
+    self.__team = team
+
+  def getTeam(self) -> int:
+    return self.__team
+
+  def __repr__(self) -> str:
+    return f"{self.__name} {self.__lastname} del grupo {self.__group} y del equipo {self.__team}"
+
+def print_stylistic_list(title: str, items: List[Any]):
+  first_character = "┌"
+  in_between_char = "│"
+  final_character = "└"
+  list_decorator = "◊"
+
+  size = len(items)
+
+  print(f"{first_character} {title}")
+
+  for value in items:
+    print(f"{in_between_char} {list_decorator} {value}")
+
+  print(f"{final_character} {size} elemento{ "" if size == 1 else "s" }")
+
+try:
+  # Estos son los datos de los integrantes del equipo
+  people = [
+    Person(name="Brian", lastname="Monteagudo Pérez", group=21, team=5),
+    Person(name="Javier David", lastname="Coroas Cintra", group=21, team=5),
+  ]
+  
+  print_stylistic_list("Personas registradas", people)
+
+except Exception as error:
+  error_message, = error.args
+  print(error_message)
