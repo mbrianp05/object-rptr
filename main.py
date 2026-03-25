d8d977de6b961c8fd95a3d31434c31c51cf1a42c
Brian <mbrianp05@gmail.com>
2026-03-25 11:30:14 -0400

    Semantic changes now more attached to the language idiomatics

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import List, Any
+from typing import List
 
 class Person:
   def __init__(self, name: str, lastname: str, group: int, team: int):
@@ -22,18 +22,18 @@ class Person:
 
   def setGroup(self, group: int):
     if group < 11:
-      raise Exception("El número del grupo debe ser como mínimo 11")
+      raise ValueError("El número del grupo debe ser como mínimo 11")
 
     group = str(group)
 
     if len(group) != 2:
-      raise Exception("El grupo debe tener 2 cifras")
+      raise ValueError("El grupo debe tener 2 cifras")
 
     if int(group[0]) > 4:
-      raise Exception("El grupo empieza por el año por lo que la primera cifra no puede ser mayor que 4")
+      raise ValueError("El grupo empieza por el año por lo que la primera cifra no puede ser mayor que 4")
 
     if int(group[1]) == 0:
-      raise Exception("El segundo dígito del grupo debe ser mayor que 0")
+      raise ValueError("El segundo dígito del grupo debe ser mayor que 0")
 
     self.__group = group
 
@@ -42,7 +42,7 @@ class Person:
   
   def setTeam(self, team: int):
     if team < 1:
-      raise Exception("El número del equipo debe ser como mínimo 1")
+      raise ValueError("El número del equipo debe ser como mínimo 1")
 
     self.__team = team
 
@@ -52,7 +52,7 @@ class Person:
   def __repr__(self) -> str:
     return f"{self.__name} {self.__lastname} del grupo {self.__group} y del equipo {self.__team}"
 
-def print_stylistic_list(title: str, items: List[Any]):
+def print_stylistic_list[T](title: str, items: List[T]):
   first_character = "┌"
   in_between_char = "│"
   final_character = "└"
@@ -76,6 +76,6 @@ try:
   
   print_stylistic_list("Personas registradas", people)
 
-except Exception as error:
-  error_message, *_ = error.args
+except ValueError as error:
+  (error_message, *_) = error.args
   print(error_message)
